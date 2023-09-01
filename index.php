c9d22d19a30920105ed162f34774362699d7ebb2
marcocampisi <marco.campisi@outlook.it>
2023-09-01 14:10:50 +0200

    Add print in page

--- index.php
+++ index.php
@@ -14,13 +14,20 @@ class Movie
 
     public function printInfo() {
         echo "Titolo: ".$this->title."<br>";
-        echo "Titolo: ".$this->genre."<br>";
-        echo "Titolo: ".$this->year."<br>";
+        echo "Genere: ".$this->genre."<br>";
+        echo "Anno: ".$this->year."<br>";
     }
 
 }
 
 $movie1 = new Movie("Memento", "Thriller", 2000);
 $movie2 = new Movie("Interstellar", "Science Fiction", 2014);
+
+echo "Film 1:<br>";
+$movie1->printInfo();
+echo "<br>";
+
+echo "Film 2:<br>";
+$movie2->printInfo();
 
 ?>
